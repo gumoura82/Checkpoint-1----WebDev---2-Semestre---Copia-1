e454e31cb0d4d171cdf3a81fdcc7f1e0b4a937bd
gumoura82 <gumoura82@gmail.com>
2024-09-06 17:03:18 -0300

    Parte 1 Sobre Completa

--- src/pages/Sobre.jsx
+++ src/pages/Sobre.jsx
@@ -3,13 +3,15 @@ import TextboxSobre from "../components/Sobre_Textbox";
 export default function Sobre() {
     return (
         <div className="Sobre">
-        <div className="SobreTxt">
-        <TextboxSobre />
-        </div>
-        <div className="SobreImg">
-        <img src="public\Viseo Merge screen 1.png" alt="" />
-        <img src="public\Saved 1.png" alt="" />
-        </div>
+            <div className="parte1sobre"> 
+                <div className="SobreTxt">
+                    <TextboxSobre />
+                </div>
+                <div className="SobreImg">
+                    <img src="public\Viseo Merge screen 1.png" className="img-1" alt="" />
+                    <img src="public\Saved 1.png" className="img-2" alt="" />
+                </div>
+            </div>
         </div>
     )
 }
