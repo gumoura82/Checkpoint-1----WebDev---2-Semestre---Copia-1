f21edc3460eb07ba307469f4891211e73fc9bfaa
ViniMata <vinicius.matareli2005@gmail.com>
2024-09-06 13:36:41 -0300

    Estilizando o Sobre

--- src/pages/Sobre.jsx
+++ src/pages/Sobre.jsx
@@ -3,9 +3,13 @@ import TextboxSobre from "../components/Sobre_Textbox";
 export default function Sobre() {
     return (
         <div className="Sobre">
+        <div className="SobreTxt">
         <TextboxSobre />
+        </div>
+        <div className="SobreImg">
         <img src="public\Viseo Merge screen 1.png" alt="" />
         <img src="public\Saved 1.png" alt="" />
+        </div>
         
         </div>
     )
